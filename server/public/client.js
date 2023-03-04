83ce4dae2b719cce278ea9b90459242a16f4279f
Bailey Fiedorowicz <bfiedorowicz1995@gmail.com>
2023-03-04 15:14:58 -0500

    changed getCalc() to display answer in same field as calculator input

--- server/public/client.js
+++ server/public/client.js
@@ -11,8 +11,8 @@ let getCalc = () => {
     axios.get('/calc').then((response) => {
         console.log(response);
         let calcsFromServer = response.data;
-        let answerDiv = document.getElementById('answer');
-        answerDiv.innerHTML = calcsFromServer[calcsFromServer.length - 1].answer;
+        let answerDiv = document.getElementById('input-field');
+        answerDiv.value = calcsFromServer[calcsFromServer.length - 1].answer;
         let historyDiv = document.getElementById('history') ;
         historyDiv.innerHTML = '';
         for (let calc of calcsFromServer) {
@@ -50,18 +50,25 @@ let submitForm = event => {
         let num1 = parseFloat(toArray[0]);
         let num2 = parseFloat(toArray[2]);
         console.log(num1);
-        let calcForServer = {
-            num1,
-            operation,
-            num2
-        };
-        axios.post('/calc', calcForServer).then((response) => {
-            console.log(response);
-            getCalc();
-        }).catch((error) => {
-            console.log(error);
-            alert('Something went wrong.');
-        });
+        console.log('num1:', num1);
+        console.log('num2:', num2);
+        if (isNaN(num1) || isNaN(num2)) {
+            alert('invalid calculation. Please try again');
+        } else {
+            let calcForServer = {
+                num1,
+                operation,
+                num2
+            };
+            axios.post('/calc', calcForServer).then((response) => {
+                console.log(response);
+                getCalc();
+            }).catch((error) => {
+                console.log(error);
+                alert('Something went wrong.');
+            });
+        }
+        
     }
 }
 
