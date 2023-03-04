e735821f8e963819292feb947af52068fefc764b
Bailey Fiedorowicz <bfiedorowicz1995@gmail.com>
2023-03-04 13:37:13 -0500

    created getAnswer, get and post requests in Calculator.router.js

---
+++ server/routes/Calculator.router.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+
+let calcs = [];
+
+function getAnswer() {
+    let calcObject = calcs[calcs.length - 1]
+    switch(calcObject.operation) {
+        case '+':
+            calcObject.answer = calcObject.num1 + calcObject.num2;
+            break;
+        case '-':
+            calcObject.answer = calcObject.num1 - calcObject.num2;
+            break;
+        case '*':
+            calcObject.answer = calcObject.num1 * calcObject.num2;
+            break;
+        case '/':
+            calcObject.answer = calcObject.num1 / calcObject.num2;
+            break;
+        default:
+            console.log('invalid calculation');
+    }
+}
+
+
+router.get('/', (req, res) => {
+    console.log('GET request made for /calc');
+    res.send(calcs);
+});
+
+router.post('/', (req, res) => {
+    console.log('POST request made for /calc');
+    console.log(req.body);
+    let calcToAdd = req.body;
+    calcs.push(calcToAdd);
+    res.sendStatus(201);
+    getAnswer();
+});
+
+
+module.exports = router;
